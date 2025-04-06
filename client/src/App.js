66d68f2379b49263843cac8eca66ea4ba564e77d
Yun Hsuan Chan <149023972+YShuan-Chan@users.noreply.github.com>
2025-04-06 02:50:56 -0700

    login/register show error

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import './App.css';
-
-// 简单的身份验证检查
-// const isAuthenticated = () => !!localStorage.getItem('token');
-
-// 保护路由的组件
-// const ProtectedRoute = ({ children }) => {
-//   if (!isAuthenticated()) {
-//     return <Navigate to="/login" />;
-//   }
-//   return children;
-// };
-
 
 
 function App() {
